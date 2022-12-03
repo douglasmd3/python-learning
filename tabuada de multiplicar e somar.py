5b0f44844da580cdd06a8e4b51de44163ac8c0d6
Michael Douglas <74698250+douglasmd3@users.noreply.github.com>
2022-12-03 02:10:20 -0300

    Consultar tabuada de fatores e totais

--- tabuada de multiplicar e somar.py
+++ tabuada de multiplicar e somar.py
@@ -1,6 +1,6 @@
 n = int(input("digite um valor: "))
 for i in range(1, 11):
     print("%.2d X %.2d = %.2d" % (n, i, i * n))
-print("~/~" * 10)
+print("~/~" * 15)
 for i in range(1, 11):
     print("%.2d + %.2d = %.2d" % (n, i, i + n))
